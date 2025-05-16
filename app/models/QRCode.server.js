3a6c49c99f2ddefa26cabf3eef105c0a4591143c
Reke <rekeojobor2@gmail.com>
2025-05-16 13:20:55 -0700

    Scanning a QR Code takes user to either product details page or checkout with the product in the cart

--- app/models/QRCode.server.js
+++ app/models/QRCode.server.js
@@ -21,3 +21,15 @@ export function getQRCodeImage(id) {
     const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
     return qrcode.toDataURL(url.href);
 }
+
+// Scanning a QR code takes the user to a specific destination (the product detail page or a checkout with the product in the cart).
+export function getDestinationURL(qrCode) {
+    if (qrCode.destination === "product") {
+        return `https://${qrCode.shop}/products/${qrCode.productHandle}`;
+    }
+
+    const match = /gid:\/\/shopify\/ProductVariant\/([0-9]+)/.exec(qrCode.productVariantId);
+    invariant(match, "Product variant ID is not valid");
+
+    return `https://${qrCode.shop}/cart/${match[1]}:1`;
+}
